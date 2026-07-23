4409ac4f312ba61c0c953df70c1d6bc407d6e861
karishma95kale <karishmakale9595@gmail.com>
2026-07-23 22:46:38 +0530

    Debug login

--- routes/user.js
+++ routes/user.js
@@ -9,14 +9,35 @@ router.get("/signup", userController.renderSignupForm);
 router.post("/signup", wrapAsync(userController.signup));
 
 router.get("/login", userController.renderLoginForm);
-router.post("/login",
-   saveRedirectUrl,
-   passport.authenticate("local", {
-      failureRedirect: "/login",
-      failureFlash: true,
-   }),
-   wrapAsync(userController.login)
-);
+router.post("/login", saveRedirectUrl, (req, res, next) => {
+    passport.authenticate("local", (err, user, info) => {
+
+        console.log("========== LOGIN DEBUG ==========");
+        console.log("Request Body:", req.body);
+        console.log("Error:", err);
+        console.log("User:", user);
+        console.log("Info:", info);
+
+        if (err) {
+            return next(err);
+        }
+
+        if (!user) {
+            req.flash("error", info?.message || "Invalid username or password");
+            return res.redirect("/login");
+        }
+
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+
+            req.user = user;
+            return userController.login(req, res);
+        });
+
+    })(req, res, next);
+});
 
 router.get("/logout", userController.logout);
 router.post("/wishlist/:hostelId", isLoggedIn, wrapAsync(userController.toggleWishlist));
